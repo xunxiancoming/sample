6ff17145ea64eef321e8a22e5c0710a5cfb2a6fe
jinlong <1475711781@qq.com>
2018-12-25 03:38:12 +0000

    移除邮箱发送的form选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -93,13 +93,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '1475711781@qq.com';
-        $name = 'king';
         $to = $user->email;
         $subject = "感谢注册sample应用！请确认您的邮箱。";
 
-        Mail::send($view,$data,function($message) use ($from,$name,$to,$subject){
-            $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view,$data,function($message) use ($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 
