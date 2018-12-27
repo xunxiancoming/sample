5a35600d753cc634e332edb524f1fb434fb6ef3a
jinlong <1475711781@qq.com>
2018-12-27 10:17:34 +0000

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,12 +4,16 @@
 @section('content')
 	
 	<div class="row">
-		<div class="col-md-offset-2 col-md-8">
+		<div class="col-md-offset-1 col-md-10">
 			<div class="col-md-12">
-				<div class="col-md-offset-2 col-md-8">
+				<div class="col-md-offset-1 col-md-10">
 					<section class="user_info">
 						@include('shared._user_info',['user'=>$user])
 					</section>
+					<section class="stats mt-4">
+						@include('shared._stats',['user'=>$user])
+					</section>
+					<hr>
 					<section>
 						@if($statuses->count()>0)
 						<ul class="list-unstyled">
