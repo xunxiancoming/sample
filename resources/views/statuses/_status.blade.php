c0a7735b065f1564bf7100e10a111788dadcbac8
jinlong <1475711781@qq.com>
2018-12-27 04:04:22 +0000

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -4,7 +4,15 @@
 	</a>
 
 	<div class="media-body">
-		<h5 class="mt-0 mb-1"><small>{{ $user->name }} / {{ $user->created_at->diffForHumans() }} </small></h5>
-		<div style="clear: both">{{ $status->content }}</div>
+		<h5 class="mt-0 mb-1 col-md-8"><small>{{ $user->name }} / {{ $user->created_at->diffForHumans() }} </small></h5>
+		<div style="clear: both" class="col-md-10">{{ $status->content }}</div>
+		@can('destroy',$status)
+		<form action="{{ route('statuses.destroy',$status->id )}}" method="POST" onsubmit="return confirm('确认删除本条微博吗？')">
+			{{ csrf_field() }}
+			{{ method_field('DELETE') }}
+			<button type="submit" class="btn btn-sm btn-danger" style="float: right;" >删除</button>
+		</form>
+		@endcan
 	</div>
+
 </li>
