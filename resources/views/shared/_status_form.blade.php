965f88224f0f9b0e646ce3e840e7eaad93604fdc
jinlong <1475711781@qq.com>
2018-12-27 03:23:06 +0000

    发布微博

---
+++ resources/views/shared/_status_form.blade.php
@@ -0,0 +1,9 @@
+<form action="{{ route('statuses.store') }}" method="POST">
+	@include('shared._errors')
+	{{csrf_field()}}
+
+	<textarea name="content" id="" placeholder="聊聊新鲜事儿……" class="form-control" rows="3">{{old('content')}}</textarea>
+	<div class="text-right">
+		<button type="submit" class="btn btn-primary mt-3">发布</button>
+	</div>
+</form>
